a5e6dc787206db119f5bf396564e2f5443ad099c
Altay-Akyurek <120604586+Altay-Akyurek@users.noreply.github.com>
2023-04-08 06:47:36 +0300

    Update visa final homework grade calculation.py

--- visa final homework grade calculation.py
+++ visa final homework grade calculation.py
@@ -1,10 +1,13 @@
 def ortalama(a,b,c):
     reply=((a*(0.4))+(b*0.1)+(c*0.5))
     if (c<50):
+        print("note %s"%(reply))
         print("you stayed")
     elif (reply<50):
+        print("note %s"%(reply))
         print("you stayed")
     else:
+        print("note %s"%(reply))
         print("you passed ")
     
 
@@ -14,6 +17,7 @@ while True:
     b=int(input("homeword grade"))
     c=int(input("final grade"))
     ortalama(a,b,c)
+    
 
 
 
