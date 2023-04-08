84e12c0bf32c09a6a52b245feeebd0cba7b554b9
Altay-Akyurek <120604586+Altay-Akyurek@users.noreply.github.com>
2023-04-08 06:38:55 +0300

    Add files via upload

---
+++ visa final homework grade calculation.py
@@ -0,0 +1,19 @@
+def ortalama(a,b,c):
+    reply=((a*(0.4))+(b*0.1)+(c*0.5))
+    if (c<50):
+        print("you stayed")
+    elif (reply<50):
+        print("you stayed")
+    else:
+        print("you passed ")
+    
+
+
+while True:
+    a=int(input("visa note:"))
+    b=int(input("homeword grade"))
+    c=int(input("final grade"))
+    ortalama(a,b,c)
+
+
+
